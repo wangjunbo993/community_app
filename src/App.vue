79d65517d96c6cf3d33266bb9bf74d1936e18572
王俊波 <wangjb@seeyon.com>
2025-12-25 16:57:54 +0800

    feat: updata sys

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+// #ifdef WEB
+import { updateShades } from '$unocss-preset-shades'
+// #endif
+
+onLaunch(() => {
+  console.log('App Launch')
+})
+onShow(() => {
+  console.log('App Show')
+})
+onHide(() => {
+  console.log('App Hide')
+})
+
+const appStore = useAppStore()
+
+// #ifdef WEB
+watchEffect(() => {
+  updateShades(appStore.primaryColor)
+})
+// #endif
+</script>
+
+<style lang="scss">
+@import '@unocss-applet/reset/uni-app/button-after.css';
+@import '@unocss-applet/reset/uni-app/tailwind-compat.css';
+@import './styles/css/index.css';
+</style>
